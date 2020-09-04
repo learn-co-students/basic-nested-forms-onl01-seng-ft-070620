e8a1576fb0a348649b941f888a0cbe661c66a05e
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-04 11:40:38 -0700

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,12 @@
 class Person < ActiveRecord::Base
   has_many :addresses
-  accepts_nested_attributes_for :addresses
+  # accepts_nested_attributes_for :addresses
+
+  def addresses_attributes=(addresses_attributes)
+    # addresses_attributes = {"0"=>{"street_address_1"=>"605 SW 36th St.", "street_address_2"=>"", "city"=>"Cape Coral", "state"=>"FL", "zipcode"=>"", "address_type"=>"work"}, "1"=>{"street_address_1"=>"6357 3rd Ave", "street_address_2"=>"", "city"=>"St Petersburg", "state"=>"FL", "zipcode"=>"", "address_type"=>"home"}}
+    addresses_attributes.values.each do |address_attributes|
+      self.addresses.build(address_attributes)
+    end
+  end
 
 end
