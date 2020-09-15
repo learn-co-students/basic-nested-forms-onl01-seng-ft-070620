9e71987ac49aecca67300ed62dc1206fc173edf3
Ethan <eleetyson@gmail.com>
2020-09-15 15:29:03 -0400

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,6 @@
 class Person < ActiveRecord::Base
   has_many :addresses
-
+  # this creates the #addresses_attributes method for us
+  # allows us to create a person object and mutltiple address objects at once
+  accepts_nested_attributes_for :addresses
 end
